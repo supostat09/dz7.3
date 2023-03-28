703159eda653436aaddd25ae46af4a7404c94f30
dzhumadil <dzhumadil.a@icloud.com>
2023-03-28 12:36:40 +0600

    Initial Commit

---
+++ dz7.3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dz7.3
+//
+//  Created by Абдулла-Бек on 28/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
